d059d75ef548c3bc5d4525fd1eb0288f35f2874e
louis-nwosu <nwachukwuchinanu@gmail.com>
2021-06-07 01:38:18 -0700

    progress

---
+++ src/components/home/notificationComp.tsx
@@ -0,0 +1,18 @@
+// import { Button, notification } from 'antd';
+
+// const openNotification = () => {
+//   notification.open({
+//     message: 'Notification Title',
+//     description:
+//       'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+//     className: 'custom-class',
+//     style: {
+//       width: 600,
+//     },
+//   });
+// };
+
+//   <Button type="primary" onClick={openNotification}>
+//     Open the notification box
+//   </Button>,
+export {}
